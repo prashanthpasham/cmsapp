f2742772c3ece780af63ad1dd4d83ff12305813f
prashanth <prashanthpasham@users.noreply.github.com>
2019-10-01 18:44:04 +0530

    Update LoginService.js

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import axios from 'axios';
 var server="http://localhost:8084/";
-//var isAuthenticated=false;
+var isAuthenticated=false;
 const routeTable=[{
   "name":"Portal Access",
   "path":"/dashboard/access-portal",
-  "component":"OrganizationAccess"
+  "component":"OrganizationPortal"
 },{
   "name":"College Info",
   "path":"/dashboard/management-info",
-  "component":"MangementInfo"
+  "component":"OrganizationPortal"
 },{
   "name":"Employee",
   "path":"/dashboard/employee-list",
   "component":"EmployeeList"
 }];
 const LoginService = {
-    //isAuthenticated: true,
+  //  isAuthenticated: false,
     isLoggedIn(){
-      if(localStorage.getItem("username")===undefined){
+     // alert(localStorage.getItem("username"))
+      if(localStorage.getItem("username")===undefined || localStorage.getItem("username")==null){
         return false;
       }else
       return true;
+      //alert(isAuthenticated);
     },
     postService(link,data){
       return axios({
@@ -39,11 +41,39 @@ const LoginService = {
         console.log(response);
     })
     },
+	fetchGet(link,token){
+		const myHeaders = new Headers();
+
+//myHeaders.append('Content-Type', 'application/json');
+myHeaders.append('token', 'Bearer '+token);
+
+		return fetch(server+link, {
+    method: 'GET',
+    headers: myHeaders,
+	mode:'no-cors'
+  }).then(function (response) {
+        //handle error
+        console.log(response);
+		  return response.json();
+    }).catch(function (response) {
+        //handle error
+        console.log(response);
+    })
+	},
     getService(link,token){
-      return axios.get(server+link, {
-    headers:{"Authorization" : `Bearer ${token}`}
-  })
-    .then(response => {return response.data})
+		//console.log('Bearer '+token);
+
+      return  axios({
+        url:server+link,
+        method:'get',
+        headers: {
+          'Authorization': 'Bearer '+token,
+		  "Access-Control-Allow-Origin":"*"
+       }
+      }) .then(function (response) {
+        //handle success
+       return response.data;
+    })
     .catch(function (response) {
         //handle error
         console.log(response);
@@ -56,24 +86,38 @@ const LoginService = {
      
     },
     signout(username) {
-     // this.isAuthenticated = false;
+      //this.isAuthenticated = false;
       localStorage.removeItem("username");
-      localStorage.removeItem("token")
+      localStorage.removeItem("token");
+      localStorage.removeItem("menus");
     },
+	getAllClients(link,token) {
+  
+
+  return axios.get(server+link, {
+    headers:{"Authorization" : `Bearer ${token}`}
+  })
+    .then(response => {return response.data})
+    .catch((error) => {
+      console.error(error);
+    });
+},
     menusService(){
      
       if(localStorage.getItem("username")!=undefined){
         if(localStorage.getItem("token")!=undefined){
-          //alert(localStorage.getItem("token"));
-          return this.getService("login/menus-user?username="+localStorage.getItem("username"),localStorage.getItem("token"))
+         // alert(localStorage.getItem("token"));
+          return this.getAllClients("login/menus-user?username="+localStorage.getItem("username"),localStorage.getItem("token"))
           .then(data=>{
-            //alert(JSON.stringify(data));
+			   var finalMenus=[];
+            alert(JSON.stringify(data));
+			if(data!=undefined){
             var map = new Map();
             routeTable.forEach(link=>{
                map.set(link.name,link);
             });
             console.log(map);
-            var finalMenus=[];
+           
             data.links.forEach(element => {
               var obj={};
               obj.name=element.menutitle;
@@ -92,6 +136,7 @@ const LoginService = {
               finalMenus.push(obj);
             });
             alert("finalMenus>>"+JSON.stringify(finalMenus));
+			}
             return finalMenus;
           });
         }
